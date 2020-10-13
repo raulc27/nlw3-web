9b52f6ee96ca56555e808749716bba5deea7baf0
Raul Castro <raulc27@gmail.com>
2020-10-12 22:58:13 -0300

    adicionando propriedade no CSS q esqueci

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -23,7 +23,7 @@ function Landing(){
         </div>
   
         <Link to="/app" className="enter-app">
-         <FiArrowRight size={26} color="rgba(0,0,0,0.6" />
+         <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
         </Link>
         </div>
      </div>
